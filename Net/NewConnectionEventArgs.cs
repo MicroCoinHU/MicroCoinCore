b26b39cccbf5c12e01663076dec65619546a9527
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2021-05-02 21:08:10 +0200

    Add files via upload

---
+++ Net/NewConnectionEventArgs.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// This file is part of MicroCoin - The first hungarian cryptocurrency
+// Copyright (c) 2018 Peter Nemeth
+// NodeServerList.cs - Copyright (c) 2018 Németh Péter
+//-----------------------------------------------------------------------
+// MicroCoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MicroCoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU General Public License for more details.
+//-----------------------------------------------------------------------
+// You should have received a copy of the GNU General Public License
+// along with MicroCoin. If not, see <http://www.gnu.org/licenses/>.
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace MicroCoin.Net
+{
+    public class NewConnectionEventArgs : EventArgs
+    {
+        public NodeServer Node { get; set; }
+
+        public NewConnectionEventArgs(NodeServer node)
+        {
+            Node = node;
+        }
+    }
+ }
