fc0ca5765664282aabb8bcb197e42bcea2684656
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2021-05-02 20:33:50 +0200

    Add files via upload
    Moved from MicroCoinClient.cs

---
+++ Net/HelloRequestEventArgs.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// This file is part of MicroCoin - The first hungarian cryptocurrency
+// Copyright (c) 2018 Peter Nemeth
+// MicroCoinClient.cs - Copyright (c) 2018 Németh Péter
+//-----------------------------------------------------------------------
+// MicroCoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MicroCoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU General Public License for more details.
+//-----------------------------------------------------------------------
+// You should have received a copy of the GNU General Public License
+// along with MicroCoin. If not, see <http://www.gnu.org/licenses/>.
+//-----------------------------------------------------------------------
+
+
+using MicroCoin.Protocol;
+using System;
+
+namespace MicroCoin.Net
+{
+    public class HelloRequestEventArgs : EventArgs
+    {
+        public HelloRequest HelloRequest { get; set; }
+        public HelloRequestEventArgs(HelloRequest helloRequest)
+        {
+            HelloRequest = helloRequest;
+        }
+    }
+
+}
