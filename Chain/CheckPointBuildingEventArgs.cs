3445aeabfddc813f24a85eca6e373020c5f8d4f1
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2021-05-02 19:43:33 +0200

    Add files via upload

---
+++ Chain/CheckPointBuildingEventArgs.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------
+// This file is part of MicroCoin - The first hungarian cryptocurrency
+// Copyright (c) 2018 Peter Nemeth
+// CheckPoints.cs - Copyright (c) 2018 Németh Péter
+//-----------------------------------------------------------------------
+// MicroCoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MicroCoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU General Public License for more details.
+//-----------------------------------------------------------------------
+// You should have received a copy of the GNU General Public License
+// along with MicroCoin. If not, see <http://www.gnu.org/licenses/>.
+//-----------------------------------------------------------------------
+
+namespace MicroCoin.Chain
+{
+    public class CheckPointBuildingEventArgs
+    {
+        public int BlocksNeeded { get; set; }
+        public int BlocksDone { get; set; }
+
+    }
+}
