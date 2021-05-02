0045b76994feb73884a61266d5407522bdfe81bb
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2021-05-02 19:50:51 +0200

    Update CheckPoints.cs

--- Chain/CheckPoints.cs
+++ Chain/CheckPoints.cs
@@ -17,7 +17,6 @@
 // along with MicroCoin. If not, see <http://www.gnu.org/licenses/>.
 //-----------------------------------------------------------------------
 
-
 using log4net;
 using MicroCoin.Transactions;
 using MicroCoin.Util;
